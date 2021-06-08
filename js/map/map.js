f58d80b2d92bf27d15798471ee0620b814fd52a1
Stefan <stefan97.nbgd@gmail.com>
2021-06-08 03:48:18 +0200

    Adding Footer Section

---
+++ js/map/map.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", (event) => {
+    //Map on Footer//
+    var mymap = L.map('mapid', {attributionControl: false}).setView([44.79174382548834, 20.477597347590308], 16);
+
+    var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoic3RlZmFubXJ2aWMiLCJhIjoiY2tpaHp3YjF0MGQ0NDJybWxuZzF6MHIzdCJ9.NIx8s8uOmLHFmBD0qSGoCw'
+    }).addTo(mymap);
+
+    var marker = L.marker([44.79143382548834, 20.477597347590308]).addTo(mymap).bindPopup("Gospodara Vučića 57 / 6");
+});
