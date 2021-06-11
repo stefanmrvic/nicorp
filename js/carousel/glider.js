2312f721fc99cfeb03b112ca8a5865dca2b9ad83
Stefan <stefan97.nbgd@gmail.com>
2021-06-11 04:09:05 +0200

    Adding responsiveness to 1000px

--- js/carousel/glider.js
+++ js/carousel/glider.js
@@ -3,5 +3,22 @@ new Glider(document.querySelector('.glider'), {
     dots: '#dots',
     draggable: true,
     scrollLock: true,
-    dragVelocity: 1.7
+    dragVelocity: 1.7,
+    responsive: [
+      {
+        // screens greater than >= 770px
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+          scrollLock: false
+        }
+      },{
+        // screens greater than >= 1000px
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2.2,
+          scrollLock: false
+        }
+      }
+    ]
   });
