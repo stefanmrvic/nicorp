b813367405820b27c211b1324e1eb8a9987bdb24
Stefan Mrvic <stefan97.nbgd@gmail.com>
2021-07-31 19:20:21 +0200

    adding last changes

--- js/progressBar/progressBar.js
+++ js/progressBar/progressBar.js
@@ -6,45 +6,39 @@ document.addEventListener('DOMContentLoaded', () => {
     window.onscroll = function() {
         let section1 = document.getElementById('hero').getBoundingClientRect().top;
         let section2 = document.getElementById('about-us').getBoundingClientRect().top;
-        let section3 = document.getElementById('services').getBoundingClientRect().top;
-        let section4 = document.getElementById('all-services').getBoundingClientRect().top;
-        let section5 = document.getElementById('projects').getBoundingClientRect().top;
-        let section6 = document.getElementById('our-clients').getBoundingClientRect().top;
-        let section7 = document.getElementById('footer').getBoundingClientRect().top;
+        let section3 = document.getElementById('all-services').getBoundingClientRect().top;
+        let section4 = document.getElementById('projects').getBoundingClientRect().top;
+        let section5 = document.getElementById('our-clients').getBoundingClientRect().top;
+        let section6 = document.getElementById('footer').getBoundingClientRect().top;
 
         if (section1 <= 302) {
-            progressBar.style.height = '30px';
+            progressBar.style.height = '35px';
             currentNum.innerHTML = '01';
         }
     
         if (section2 <= 302) {
-            progressBar.style.height = '60px';
+            progressBar.style.height = '70px';
             currentNum.innerHTML = '02';
         }
     
         if (section3 <= 302) {
-            progressBar.style.height = '90px';
+            progressBar.style.height = '105px';
             currentNum.innerHTML = '03';
         }
     
         if (section4 <= 302) {
-            progressBar.style.height = '120px';
+            progressBar.style.height = '140px';
             currentNum.innerHTML = '04';
         }
     
         if (section5 <= 302) {
-            progressBar.style.height = '150px';
+            progressBar.style.height = '175px';
             currentNum.innerHTML = '05';
         }
     
         if (section6 <= 302) {
-            progressBar.style.height = '180px';
-            currentNum.innerHTML = '06';
-        }
-    
-        if (section7 <= 302) {
             progressBar.style.height = '210px';
-            currentNum.innerHTML = '07';
+            currentNum.innerHTML = '06';
         }
     }
 });
